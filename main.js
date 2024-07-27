0b4fcd5d0b38fbb7f66b7caa0d807b9eb198f7d9
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-07-27 23:05:44 +0530

    restructured code

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
   res.send("dockermon API!");
 });
 
+// api to test stream
+app.get("/teststream", (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader("Transfer-Encoding", "chunked");
+  res.write("hello\n");
+  setTimeout(() => {
+    res.write("world\n");
+    setTimeout(() => {
+      res.end("end\n");
+    }, 1000);
+  }, 1000);
+});
+
 app.get("/containers", (req, res) => {
   docker.listContainers(function (err, containers) {
     res.json({ containers });
@@ -55,18 +68,7 @@ app.get("/containers/:id/logs", (req, res) => {
   );
 });
 
-// api to test stream
-app.get("/teststream", (req, res) => {
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  res.setHeader("Transfer-Encoding", "chunked");
-  res.write("hello\n");
-  setTimeout(() => {
-    res.write("world\n");
-    setTimeout(() => {
-      res.end("end\n");
-    }, 1000);
-  }, 1000);
-});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
